bc5817e7ebc9462eb3e4eed06141496c6502a44f
troychaplin <troy.chaplin@gmail.com>
2025-06-12 21:38:14 -0400

    change: theme

--- wordpress/wp-content/themes/monorepo-theme/webpack.config.js
+++ wordpress/wp-content/themes/monorepo-theme/webpack.config.js
@@ -1,16 +1,16 @@
 /**
  * WordPress Webpack Config
  */
-const path = require("path");
-require("dotenv").config();
+const path = require('path');
+require('dotenv').config();
 
 module.exports = {
 	entry: {
-		scripts: [path.resolve(__dirname, "src/scripts.js")],
+		scripts: [path.resolve(__dirname, 'src/scripts.js')],
 	},
 	output: {
-		path: path.resolve(__dirname, "build"),
-		filename: "[name].js",
+		path: path.resolve(__dirname, 'build'),
+		filename: '[name].js',
 	},
 	module: {
 		rules: [
@@ -18,9 +18,9 @@ module.exports = {
 				test: /\.m?js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: "babel-loader",
+					loader: 'babel-loader',
 					options: {
-						presets: [["@babel/preset-env", { targets: "defaults" }]],
+						presets: [['@babel/preset-env', { targets: 'defaults' }]],
 					},
 				},
 			},
