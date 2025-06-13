570eb2ec59c6451e94a68f52f43606235b055f07
troychaplin <troy.chaplin@gmail.com>
2025-06-13 11:16:33 -0400

    change: sass for theme

---
+++ wordpress/wp-content/themes/monorepo-theme/src/scripts.js
@@ -0,0 +1,4 @@
+// import './styles.scss';
+// import './editor-styles.scss';
+
+console.log('Monorepo theme scripts loaded');
--- wordpress/wp-content/themes/monorepo-theme/webpack.config.js
+++ wordpress/wp-content/themes/monorepo-theme/webpack.config.js
@@ -3,27 +3,30 @@
  */
 const path = require('path');
 require('dotenv').config();
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 module.exports = {
 	entry: {
 		scripts: [path.resolve(__dirname, 'src/scripts.js')],
+		styles: [path.resolve(__dirname, 'src/styles.scss')],
 	},
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: '[name].js',
+		clean: true,
 	},
 	module: {
 		rules: [
 			{
-				test: /\.m?js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [['@babel/preset-env', { targets: 'defaults' }]],
-					},
-				},
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 			},
 		],
+	},
+	plugins: [new RemoveEmptyScriptsPlugin(), new MiniCssExtractPlugin({ filename: '[name].css' })],
+	optimization: {
+		removeEmptyChunks: true,
+		splitChunks: false,
 	},
 };
